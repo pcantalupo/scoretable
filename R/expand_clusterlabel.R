a8758de8ab95d33259843076766b30eb4ab57183
Paul Cantalupo <pcantalupo@gmail.com>
2025-12-04 09:04:57 -0500

    renaming deconvolute_clusterlable to expand_clusterlabel

--- R/expand_clusterlabel.R
+++ R/expand_clusterlabel.R
@@ -1,9 +1,10 @@
 # Purpose: You have cluster labels (in your cluster scores table) and you want to expand these labels to the cell level so you can add them to a table of cell metadata
 # scores - data.frame of cluster scores containing at least two columns: 'clusters' and 'labels' (rows are clusters)
 # clusters - vector that specifies the cluster for each cell 
-deconvolute_clusterlabel = function(scores, clusters) {
+expand_clusterlabel = function(scores, clusters) {
   return(scores$labels[match(clusters, scores$clusters)]) 
 }
+
 
 
 
