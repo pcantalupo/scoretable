bcea6551f4f3e50db895ef33e167fc1858ab4487
Paul Cantalupo <pcantalupo@gmail.com>
2025-12-05 09:18:05 -0500

    updating test

--- test.R
+++ test.R
@@ -1,83 +1,90 @@
 pacman::p_load(tidyverse)
 
 
-# Create a matrix of scores 'm' (no row nor colnames)
+##########################################
+# Observation level ------------------------#
+# Create a matrix of scores 'm' (no row nor colnames) - 20 rows, 3 columns
 set.seed(15)
-m = matrix(rnorm(30), nrow = 10)
+m = matrix(rnorm(60), nrow = 20)
+m
 
-# Data Frame
+### Data Frame
 scores = as.data.frame(m)
-colnames(scores)  # default V1, etc... colnames are added 
-rownames(scores)  # default 1, 2, etec... rownamew are added
+colnames(scores)  # default V1, etc... colnames are added by default
+rownames(scores)  # default 1, 2, etec... rownamew are added by default
 
 colnames(scores) = NULL
 rownames(scores) = NULL   # rownames are never eliminated with a dataframe
 scores
 
-(result = add_labels_based_on_max(scores))
+# Annotate (adds clusters, labels, score)
+(pred = add_labels_based_on_max(scores))  # should give error due to colnames missing
 
-colnames(scores) = c("TCell","BCell","Macro")
-(result = add_labels_based_on_max(scores))
-
-rownames(scores) = paste0("c", rep(1:nrow(scores)))
-(result = add_labels_based_on_max(scores))
+colnames(scores) = c("TCell","BCell","Macro")  # add colnames
+(pred = add_labels_based_on_max(scores))     # success
 
 
-# Matrix
+
+### Matrix
 scores = m
-colnames(scores)
-rownames(scores)
+colnames(scores); rownames(scores)
+scores
 
-(result = add_labels_based_on_max(scores))
+# Annotate
+(pred = add_labels_based_on_max(scores)) # error because both row and col names are missing
 
-colnames(scores) = c("TCell","BCell","Macro")
-(result = add_labels_based_on_max(scores))
+colnames(scores) = c("TCell","BCell","Macro")  # add colname but still error b/c rownames missing
+(pred = add_labels_based_on_max(scores))
 
-rownames(scores) = paste0("c", rep(1:nrow(scores)))
-(result = add_labels_based_on_max(scores))
-
-
-# Unknown test
-(result = add_labels_based_on_max(scores, unknown = TRUE)) # default cutoff 0.25
-(result = add_labels_based_on_max(scores, unknown = TRUE, cutoff = 0.45))
+rownames(scores) = paste0("c", rep(1:nrow(scores))) # add row names 
+(pred = add_labels_based_on_max(scores))  # success
 
 
+# Annotate - Unknown test
+(pred = add_labels_based_on_max(scores, unknown = TRUE)) # default cutoff 0.25
+(pred = add_labels_based_on_max(scores, unknown = TRUE, cutoff = 0.45))
 
 
-
-# Clusters level ------------------------#
-clusters = c(rep(0,5),rep(1,5)); clusters   # define two clusters 0 and 1
-
-scores = as.data.frame(m)
-colnames(scores) = c("TCell","BCell","Macro")
-
-# aggregate single cell scores to cluster level
-cluster.scores = scores_clusterlevel(scores, clusters)
-cluster.scores
-
-# annotate clusters (adds two columns 'clusters' and 'labels')
-cluster.scores.labels = add_labels_based_on_max(cluster.scores)
-cluster.scores.labels
-
-# annotate clusters with Unknowns based on a cutoff
-add_labels_based_on_max(cluster.scores, unknown = TRUE, cutoff = 0.35)
-
-deconvolute_clusterlabel(cluster.scores.labels, clusters)
-
-
-
-# Single cell level ------------------------#
-scores.labels = add_labels_based_on_max(scores)
-scores.labels
-table(scores.labels$labels)
-
+### Plotting
+(pred = add_labels_based_on_max(scores))
+table(pred$labels)
 
 # Plot scores by Pred
-plot_scores_by_pred(scores.labels, jitter = TRUE)
+plot_scores_by_pred(pred, jitter = TRUE)
 
 # Plot signal vs noise
-plot_signal_vs_noise(scores.labels)
-plot_signal_vs_noise(scores.labels, jitter = TRUE)
+plot_signal_vs_noise(pred)
+plot_signal_vs_noise(pred, jitter = TRUE)
+
+
+
+
+
+##########################################
+# Clusters level ------------------------#
+#m = matrix(rnorm(100), nrow = 20, ncol = 5)
+set.seed(1)  # need seed 1 so that when unknowns are determined there is one cluster per label
+clusters = sample(0:3, 20, replace=TRUE)
+table(clusters)
+
+
+### DataFrame
+scores = as.data.frame(m)
+colnames(scores) = c("TCell", "BCell", "Macro")
+
+# Aggregate observations to cluster level
+(scores.cluster = scores_clusterlevel(scores, clusters))
+
+# Annotate
+(pred = add_labels_based_on_max(scores.cluster))
+# Annotate - Unknown test
+(pred = add_labels_based_on_max(scores.cluster, unknown = TRUE, cutoff = 0.41))
+
+# Expand cluster labels to observation level
+expand_clusterlabel(pred, clusters)
+
+
+
 
 
 
